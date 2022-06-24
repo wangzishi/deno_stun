a083790f2d7713ef26293c404f42b95aac6f5cca
Wang Zishi <ynh.2@outlook.com>
2022-06-24 22:10:56 +0800

    feat: server copy input data to output data

--- server.ts
+++ server.ts
@@ -1,21 +1,34 @@
 import { copy } from "https://deno.land/std@0.144.0/streams/conversion.ts";
 
 // IANA: https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml?search=stun
-const STUN_PORT = 3478;
+export const STUN_PORT = 3478;
+const MTU = 548;
 // const STUNS_PORTS = 5349;
 
 export class Server {
+  #listener?: Deno.Listener;
   // constructor() {}
 
   async start() {
     const listener = Deno.listen({ port: STUN_PORT });
     console.log(`Listening on port ${STUN_PORT}`);
     console.log(listener.addr);
+
+    this.#listener = listener;
+
+    const buf = new Uint8Array(MTU);
+
     for await (const conn of listener) {
-      copy(conn, conn).finally(() => conn.close());
+      const n = await conn.read(buf) ?? 0;
+      console.log(buf.slice(0, n));
+
+      await conn.write(buf.slice(0, n));
+      conn.close();
+      // copy(conn, conn).finally(() => conn.close());
     }
   }
-}
 
-// const server = new Server();
-// server.start();
+  stop() {
+    this.#listener?.close();
+  }
+}
